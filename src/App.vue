75775a97038dc3a224748619b512df60477a9978
小倉一泰 <kazuhiro.ogura.00@shiftinc.jp>
2022-03-22 19:15:35 +0900

    Add Test

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
-  <div>Hello XXXXX {{ value }}</div>
+  <div>Hello {{ value }}</div>
+  <div class="col-sm12">
+    <label>足し算:</label>
+    <input type="number" v-model.number="input1_1"> + 
+    <input type="number" v-model.number="input1_2"> = 
+    <span>{{ add1 }}</span>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
   data() {
+    input1_1:0,
+    input1_2:0,
     return {
       value: "World"
     };
-  }
+  },
+  computed: {
+    add1: function() {return this.input1_1 + this.input1_2}
+  },
 };
 </script>
