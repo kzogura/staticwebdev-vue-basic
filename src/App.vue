89b745fd50cab639796b1a400f81ea7f43b797ac
小倉一泰 <kazuhiro.ogura.00@shiftinc.jp>
2022-03-30 11:27:11 +0900

    debug

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>Hello {{ value }}</div>
-  <div class="col-sm12">
-    <label>足し算:</label>
-    <input type="number" v-model.number="input1_1"> + 
-    <input type="number" v-model.number="input1_2"> = 
-    <span>{{ add1 }}</span>
+  <div>Hello {{ value }}
+    <div class="col-sm12">
+      <label>足し算:</label>
+      <input type="number" v-model.number="input1_1"> + 
+      <input type="number" v-model.number="input1_2"> = 
+      <span id='addval'>{{ add1 }}</span>
+    </div>
+    <div class="col-sm12">
+      <label>割り算:</label>
+      <input type="number" v-model.number="input2_1"> + 
+      <input type="number" v-model.number="input2_2"> = 
+      <span id='divval'>{{ div1 }}</span>
+    </div>
   </div>
 </template>
 
@@ -12,14 +19,17 @@
 export default {
   name: "App",
   data() {
-    input1_1:0,
-    input1_2:0,
     return {
-      value: "World"
-    };
+      input1_1:0,
+      input1_2:0,
+      input2_1:0,
+      input2_2:0,
+      value: "World",
+    }
   },
   computed: {
-    add1: function() {return this.input1_1 + this.input1_2}
+    add1: function() {return this.input1_1 + this.input1_2},
+    div1: function() {return this.input2_1 / this.input2_2}
   },
 };
 </script>
